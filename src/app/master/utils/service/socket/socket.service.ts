5d2dcedf52892d2b8650c0d6976dd7eb0bc1ee11
SAGE-DESA <guido.garate@gmail.com>
2019-12-09 16:44:58 -0400

    realizar mer

--- src/app/master/utils/service/socket/socket.service.ts
+++ src/app/master/utils/service/socket/socket.service.ts
@@ -14,11 +14,19 @@ export class SocketService {
     this.socket.on("connect", () => {
       console.log("conectado al servidor");
       this.socketStatus = true;
+      this.salir("macc");
     });
 
     this.socket.on("disconnect", () => {
       console.log("desconectado del servidor");
       this.socketStatus = false;
+    });
+  }
+
+  salir(nombre: string) {
+    console.log("salir llamdo");
+    this.socket.emit("salir", { nombre }, resp => {
+      console.log("saliendo-angular");
     });
   }
 }
