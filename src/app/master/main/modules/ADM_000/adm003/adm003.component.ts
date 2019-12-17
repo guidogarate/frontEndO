0e38cd0c90263fd71e69d8e3cc1beb26f38689e0
djnimrod <valcyum.085@gmail.com>
2019-12-17 17:57:56 -0400

    terminando segundo modal de Periodos

--- src/app/master/main/modules/ADM_000/adm003/adm003.component.ts
+++ src/app/master/main/modules/ADM_000/adm003/adm003.component.ts
@@ -11,6 +11,23 @@ declare function initModal();
 export class Adm003Component implements OnInit {
 
   Lista: any;
+  editar: boolean = false;
+  mesInicial: string = "1";
+  mesFinal: string = "1";
+  listaMeses = [
+    { id: 1, name: "Enero" },
+    { id: 2, name: "Febrero" },
+    { id: 3, name: "Marzo" },
+    { id: 4, name: "Abril" },
+    { id: 5, name: "Mayo" },
+    { id: 6, name: "Junio" },
+    { id: 7, name: "Julio" },
+    { id: 8, name: "Agosto" },
+    { id: 9, name: "Septiembre" },
+    { id: 10, name: "Octubre" },
+    { id: 11, name: "Noviembre" },
+    { id: 12, name: "Diciembre" }
+  ];
  
   constructor() { }
 
