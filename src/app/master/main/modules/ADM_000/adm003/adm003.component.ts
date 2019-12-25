f1832777b9ab17c7c19d9b00f7b9df7e9c8f46e7
miguel angel cayo carvajal <miguelcayocarvajal37@gmail.com>
2019-12-25 16:56:16 -0400

    en

---
+++ src/app/master/main/modules/ADM_000/adm003/adm003.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adm003',
+  templateUrl: './adm003.component.html',
+  styleUrls: ['./adm003.component.css']
+})
+export class Adm003Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/master/main/modules/ADM_000/adm_000.module.ts
+++ src/app/master/main/modules/ADM_000/adm_000.module.ts
@@ -4,12 +4,13 @@ import { CommonModule, DatePipe } from "@angular/common";
 
 import { ADM_000_MODULES_ROUTES } from "./adm_000.routes";
 import { Adm001Component } from "./adm001/adm001.component";
-import { Adm002Component } from './adm002/adm002.component';
-import { Adm001Service } from '../../../utils/service/ADM-001/Adm001.service';
+import { Adm002Component } from "./adm002/adm002.component";
+import { Adm001Service } from "../../../utils/service/ADM-001/Adm001.service";
+import { Adm003Component } from "./adm003/adm003.component";
 
 @NgModule({
-  declarations: [Adm001Component, Adm002Component],
-  providers: [Adm001Service,DatePipe],
+  declarations: [Adm001Component, Adm002Component, Adm003Component],
+  providers: [Adm001Service, DatePipe],
   imports: [CommonModule, ADM_000_MODULES_ROUTES, FormsModule]
 })
 export class ADM00Module {}
