f9afd5ea3f1f65b51d298acf913d8ff1ec941323
macc001 <miguelcayocarvajal37@gmail.com>
2020-01-24 12:00:33 -0400

    24/01/2020

--- src/app/master/main/modules/modules.routes.ts
+++ src/app/master/main/modules/modules.routes.ts
@@ -16,34 +16,23 @@ const componente = [
   }
 ];
 
-let menu = JSON.parse(sessionStorage.getItem("menu"));
+const menu = JSON.parse(sessionStorage.getItem("menu"));
 const menuR: Routes = [];
-
 if (menu === null) {
+  // PONER PATH EN BIENVENIDO O PONER UN MENSAJE DE ALERTA
   menuR.push({
     path: "90",
     component: ModulesComponent,
     loadChildren: () =>
       import("./ADM_000/adm_000.module").then(mod => mod.ADM00Module)
   });
-  setTimeout(() => {
-    menu = JSON.parse(sessionStorage.getItem("menu"));
-    console.log(menu);
-    for (let index = 0; index < menu.length; index++) {
-      const result = componente.find(compon => compon.path === menu[index].id);
-      if (result !== undefined) {
-        // PARA PRUEBA AQUI COLOCAR  path: "91",
-        menuR.push(result);
-      }
-    }
-  }, 3000);
 } else {
   for (let index = 0; index < menu.length; index++) {
     const result = componente.find(compon => compon.path === menu[index].id);
     if (result !== undefined) {
+      result.path = menu[index].ruta;
       menuR.push(result);
     }
   }
 }
-
 export const MODULES_ROUTES = RouterModule.forChild(menuR);
