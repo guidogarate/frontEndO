3ee4dee99ee101a2d9dd658d82f11368db675c97
macc001 <miguelcayocarvajal37@gmail.com>
2020-01-29 17:06:39 -0400

    loading

---
+++ src/assets/elementos/js/custom.js
@@ -0,0 +1,12 @@
+/*
+Template Name: Admin Pro Admin
+Author: Wrappixel
+Email: niravjoshi87@gmail.com
+File: js
+*/
+$(function() {
+  "use strict";
+  $(function() {
+    $(".preloader").fadeOut();
+  });
+});
