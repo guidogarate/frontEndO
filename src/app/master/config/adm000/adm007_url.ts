5314ca6908b74ced4c8fd8b6ad806a72c2541a88
djnimrod <valcyum.085@gmail.com>
2020-03-04 17:16:35 -0400

    space prettier

--- src/app/master/config/adm000/adm007_url.ts
+++ src/app/master/config/adm000/adm007_url.ts
@@ -1,8 +1,6 @@
 const adm0007 = {
-    
-    get_datos_empresa : "adm_000/adm_007/get-datos-empresa/",
-    insert_datos_empresa : "adm_000/adm_007/insert-datos-empresa",
-    delete_datos_empresa : "adm_000/adm_007/delete-datos-empresa/"
-
+  get_datos_empresa: "adm_000/adm_007/get-datos-empresa/",
+  insert_datos_empresa: "adm_000/adm_007/insert-datos-empresa",
+  delete_datos_empresa: "adm_000/adm_007/delete-datos-empresa/"
 };
 export default adm0007;
