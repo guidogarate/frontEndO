49f4fec868c38e7af3908ef91fe2c44cfbbe9a26
djnimrod <valcyum.085@gmail.com>
2020-03-04 17:14:45 -0400

    prueba back

--- src/app/master/config/adm000/adm007_url.ts
+++ src/app/master/config/adm000/adm007_url.ts
@@ -1,8 +1,7 @@
 const adm0007 = {
-    
-    get_datos_empresa : "adm_000/adm_007/get-datos-empresa/",
-    insert_datos_empresa : "adm_000/adm_007/insert-datos-empresa",
-    delete_datos_empresa : "adm_000/adm_007/delete-datos-empresa/"
-
+  get_datos_empresa: "adm_000/adm_007/get-datos-empresa/",
+  insert_datos_empresa: "adm_000/adm_007/insert-datos-empresa",
+  delete_datos_empresa: "adm_000/adm_007/delete-datos-empresa/",
+  insert_logo_empresa: "adm_000/adm_007/insert-logo-empresa"
 };
 export default adm0007;
